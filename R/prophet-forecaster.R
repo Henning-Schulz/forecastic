76f6e510dabdc11096ecb3d3ec0838d6e63e62fe
Henning Schulz <henning.schulz@novatec-gmbh.de>
2019-08-25 18:58:50 +0200

    CONT-248: Improved method and field visibility of forecaster classes.

--- R/prophet-forecaster.R
+++ R/prophet-forecaster.R
@@ -7,70 +7,76 @@ library(prophet)
 #'
 #' Forecaster implementation using the telescope tool.
 #'
-ProphetForecaster <- R6Class("ProphetForecaster", inherit = Forecaster, list(
-  
-  logger = Logger$new("ProphetForecaster"),
-  
-  #' Forecasts one intensity group.
-  #' 
-  #' @param group The group name as string.
-  #' @param past The past data as data frame (columns \code{ds}, one per group intensity, one per context variable).
-  #' @param future The future data Needs to have the same columns as \code{past} (except for intensity).
-  #' @param context_variables The context variables occurring in \code{past} and \code{future}.
-  forecast_group = function(group, past, future, context_variables) {
-    m <- prophet()
+ProphetForecaster <- R6Class("ProphetForecaster", inherit = Forecaster,
+                             
+  private = list(
     
-    for (variable in context_variables) {
-      m <- add_regressor(m, variable)
+    logger = Logger$new("ProphetForecaster"),
+    
+    #' Forecasts one intensity group.
+    #' 
+    #' @param group The group name as string.
+    #' @param past The past data as data frame (columns \code{ds}, one per group intensity, one per context variable).
+    #' @param future The future data Needs to have the same columns as \code{past} (except for intensity).
+    #' @param context_variables The context variables occurring in \code{past} and \code{future}.
+    forecast_group = function(group, past, future, context_variables) {
+      m <- prophet()
+      
+      for (variable in context_variables) {
+        m <- add_regressor(m, variable)
+      }
+      
+      m <- fit.prophet(m, past %>% select(ds, y = !!group, one_of(context_variables)))
+      forecast <- predict(m, future)
+      
+      # plot(m, forecast)
+      # prophet_plot_components(m, forecast)
+      
+      forecast %>%
+        select(
+          ds,
+          !!group := yhat
+        )
     }
-    
-    m <- fit.prophet(m, past %>% select(ds, y = !!group, one_of(context_variables)))
-    forecast <- predict(m, future)
-    
-    # plot(m, forecast)
-    # prophet_plot_components(m, forecast)
-    
-    forecast %>%
-      select(
-        ds,
-        !!group := yhat
-      )
-  },
+  ),
+                             
+  public = list(
   
-  #' Does the forecast using the prophet tool.
-  do_forecast = function(context, horizon) {
-    context_variables <- self$past_intensities %>%
-      select(-timestamp, -starts_with("intensity")) %>%
-      colnames()
-    
-    forecast_start <- self$end_past + self$resolution
-    
-    past <- self$past_intensities %>%
-      mutate(ds = as_datetime(timestamp / 1000)) %>%
-      select(-timestamp)
-    
-    future <- context %>%
-      fill_context(forecast_start, horizon, self$resolution) %>%
-      mutate(ds = as_datetime(timestamp / 1000)) %>%
-      select(-timestamp)
-    
-    # forecast each group and join the results
-    groups <- self$past_intensities %>%
-      select(starts_with("intensity")) %>%
-      names()
-    
-    self$logger$info("Warn message about \"Unknown or uninitialised column: 'y'\" is OK!")
-    
-    self$forecast <- groups %>%
-      map(self$forecast_group,
-          past = past, future = future,
-          context_variables = context_variables) %>%
-      reduce(left_join) %>%
-      mutate(timestamp = as.numeric(ds) * 1000) %>%
-      select(timestamp, starts_with("intensity"))
-    
-    self$logger$info("Forecasting done.")
-  }
-  
-))
+    #' Does the forecast using the prophet tool.
+    do_forecast = function(context, horizon) {
+      context_variables <- self$past_intensities %>%
+        select(-timestamp, -starts_with("intensity")) %>%
+        colnames()
+      
+      forecast_start <- self$end_past + self$resolution
+      
+      past <- self$past_intensities %>%
+        mutate(ds = as_datetime(timestamp / 1000)) %>%
+        select(-timestamp)
+      
+      future <- context %>%
+        fill_context(forecast_start, horizon, self$resolution) %>%
+        mutate(ds = as_datetime(timestamp / 1000)) %>%
+        select(-timestamp)
+      
+      # forecast each group and join the results
+      groups <- self$past_intensities %>%
+        select(starts_with("intensity")) %>%
+        names()
+      
+      private$logger$info("Warn message about \"Unknown or uninitialised column: 'y'\" is OK!")
+      
+      self$forecast <- groups %>%
+        map(private$forecast_group,
+            past = past, future = future,
+            context_variables = context_variables) %>%
+        reduce(left_join) %>%
+        mutate(timestamp = as.numeric(ds) * 1000) %>%
+        select(timestamp, starts_with("intensity"))
+      
+      private$logger$info("Forecasting done.")
+    }
+  )
+
+)
 
--- R/telescope-forecaster.R
+++ R/telescope-forecaster.R
@@ -12,74 +12,80 @@ library(tidyverse)
 #'
 #' Forecaster implementation using the telescope tool.
 #' 
-TelescopeForecaster <- R6Class("TelescopeForecaster", inherit = Forecaster, list(
-  
-  logger = Logger$new("TelescopeForecaster"),
-  
-  #' Forecasts one intensity group.
-  #' 
-  #' @param group The group name as string.
-  #' @param past_context The past context as matrix (one context variable per column).
-  #' @param future_context The future context. Needs to have the same columns as \code{past_context}.
-  #' @param future_timestamps The future timestamps as vector. Needs to have the same length as \code{future_context}.
-  forecast_group = function(group, past_context, future_context, future_timestamps) {
-    self$logger$info("Forecasting group ", group, "...")
+TelescopeForecaster <- R6Class("TelescopeForecaster", inherit = Forecaster,
+                               
+  private = list(
     
-    tvp <- self$past_intensities[[group]]
+    logger = Logger$new("TelescopeForecaster"),
     
-    if (is_empty(past_context)) {
-      forecast <- telescope.forecast(tvp = tvp, horizon = length(future_timestamps))
-    } else {
-      forecast <- telescope.forecast(tvp = tvp, horizon = length(future_timestamps),
-                                     hist.covar = past_context,
-                                     future.covar = future_context)
+    #' Forecasts one intensity group.
+    #' 
+    #' @param group The group name as string.
+    #' @param past_context The past context as matrix (one context variable per column).
+    #' @param future_context The future context. Needs to have the same columns as \code{past_context}.
+    #' @param future_timestamps The future timestamps as vector. Needs to have the same length as \code{future_context}.
+    forecast_group = function(group, past_context, future_context, future_timestamps) {
+      private$logger$info("Forecasting group ", group, "...")
+      
+      tvp <- self$past_intensities[[group]]
+      
+      if (is_empty(past_context)) {
+        forecast <- telescope.forecast(tvp = tvp, horizon = length(future_timestamps))
+      } else {
+        forecast <- telescope.forecast(tvp = tvp, horizon = length(future_timestamps),
+                                       hist.covar = past_context,
+                                       future.covar = future_context)
+      }
+      
+      tibble(
+        timestamp = future_timestamps,
+        !!group := forecast$mean
+      )
     }
+  ),                      
+
+  public = list(
     
-    tibble(
-      timestamp = future_timestamps,
-      !!group := forecast$mean
-    )
-  },
-  
-  #' Does the forecast using the telescope tool.
-  do_forecast = function(context, horizon) {
-    self$logger$info("Forecasting the intensities...")
-    
-    # calculating past context
-    past_context <- self$past_intensities %>%
-      select(-timestamp, -starts_with("intensity")) %>%
-      as.matrix()
-    
-    # calculating the future context, removing the variables that do no occur in the past
-    forecast_start <- self$end_past + self$resolution
-    
-    if (ncol(past_context) == 0) {
-      context <- context %>%
-        select(timestamp)
-    } else {
-      context <- context %>%
-        select(timestamp, one_of(colnames(past_context)))
+    #' Does the forecast using the telescope tool.
+    do_forecast = function(context, horizon) {
+      private$logger$info("Forecasting the intensities...")
+      
+      # calculating past context
+      past_context <- self$past_intensities %>%
+        select(-timestamp, -starts_with("intensity")) %>%
+        as.matrix()
+      
+      # calculating the future context, removing the variables that do no occur in the past
+      forecast_start <- self$end_past + self$resolution
+      
+      if (ncol(past_context) == 0) {
+        context <- context %>%
+          select(timestamp)
+      } else {
+        context <- context %>%
+          select(timestamp, one_of(colnames(past_context)))
+      }
+      
+      filled_context <- context %>%
+        fill_context(forecast_start, horizon, self$resolution)
+      
+      future_context <- filled_context %>%
+        select(-timestamp) %>%
+        as.matrix()
+      
+      # forecast each group and join the results
+      groups <- self$past_intensities %>%
+        select(starts_with("intensity")) %>%
+        names()
+      
+      self$forecast <- groups %>%
+        map(private$forecast_group,
+            past_context = past_context, future_context = future_context,
+            future_timestamps = filled_context$timestamp) %>%
+        reduce(left_join)
+      
+      private$logger$info("Forecasting done.")
     }
-    
-    filled_context <- context %>%
-      fill_context(forecast_start, horizon, self$resolution)
-    
-    future_context <- filled_context %>%
-      select(-timestamp) %>%
-      as.matrix()
-    
-    # forecast each group and join the results
-    groups <- self$past_intensities %>%
-      select(starts_with("intensity")) %>%
-      names()
-    
-    self$forecast <- groups %>%
-      map(self$forecast_group,
-          past_context = past_context, future_context = future_context,
-          future_timestamps = filled_context$timestamp) %>%
-      reduce(left_join)
-    
-    self$logger$info("Forecasting done.")
-  }
-  
-))
+  )
+
+)
